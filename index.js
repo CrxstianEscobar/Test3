ac4878e09d1d90cdfea006e02e9e3460b76bc76f
Cristian Escobar <cristianyhacson2008@gmail.com>
2024-12-14 02:39:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando CrowBot ✰');
+console.log('\nდ Iniciando Sistema...');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('CrowBot\nBot', {
+say('Shadow Bot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Niño Piña`, {
+say(`By Cristian Escobar`, {
 font: 'console',
 align: 'center',
 colors: ['magenta']
@@ -57,7 +57,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('🎈 Error:\n', code);
+console.error('🤍 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -77,4 +77,4 @@ console.warn('💎 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
-start('starcore.js');
+start('heavenly.js');
